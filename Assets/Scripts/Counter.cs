ceba8556a19bc2d0cb66d47f3e49bb1f0aee66dd
Karolina Wieczorek <karolinachrop@gmail.com>
2021-01-26 21:23:38 +0100

    Developed level 2

---
+++ Assets/Scripts/Counter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+
+    //public GameObject heartsObject;
+    public Text count;
+    public int numberOfDiffrentHearts;
+    private int lastnumberOfAllHearts;
+    void Start()
+    {
+        lastnumberOfAllHearts = GameObject.Find("Hearts").transform.childCount;
+        int numberOfHeartsInOneColor = lastnumberOfAllHearts / numberOfDiffrentHearts;
+        count.text = numberOfHeartsInOneColor.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var hearts = GameObject.Find("Hearts");
+        if (lastnumberOfAllHearts != hearts.transform.childCount)
+        {
+            lastnumberOfAllHearts--;
+            count.text = (int.Parse(count.text)-1).ToString();
+        }
+    }
+}
